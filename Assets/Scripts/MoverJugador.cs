252e3e186976de4b4597f8d2cc4cf649424fef86
JackDevOs <osjack01@gmail.com>
2023-09-22 22:45:38 -0600

    actualizacion scripts

--- Assets/Scripts/MoverJugador.cs
+++ Assets/Scripts/MoverJugador.cs
@@ -7,6 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
 {
     public float velocidad = 0f;
     public GameObject obj1;
+    public GameObject obj2;
 
     // Start is called before the first frame update
     void Start()
@@ -22,14 +23,35 @@ public class NewBehaviourScript : MonoBehaviour
 
         float s = Input.GetAxis("Jump");
 
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            s = -10;
+        }
 
         Vector3 movimiento = new Vector3(mh, s, mv);
 
         transform.Translate(movimiento * velocidad * Time.deltaTime);
 
+        
+        //Objeto Cubo
         if (Input.GetKeyDown(KeyCode.P))
         {
             Instantiate(obj1, transform.position + transform.forward, Quaternion.identity);
         }
+
+        //Objeto bala
+        if (Input.GetKeyDown(KeyCode.RightControl))
+        {
+            var objBala = Instantiate(obj2, transform.position + transform.forward * 2, Quaternion.identity);
+
+            objBala.GetComponent<moverBala>().Inicializar(transform.forward );
+
+            Destroy(objBala, 10);
+
+        }
+
+       
+
     }
+    
 }
